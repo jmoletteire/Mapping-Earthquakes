7b67b6e7270feef50274bf6c13b94f9c5d185bce
jmoletteire <jmoletteire@ufl.edu>
2022-09-08 12:32:59 -0400

    plotted multiple points and binded popups using GeoJSON data.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,5 +1,5 @@
 // Create the map object with center at the San Francisco airport
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -21,19 +21,25 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-// Grabbing our GeoJSON data
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr><h3>Airport name: ${feature.properties.name}</h3>`);
-    }
-}).addTo(map);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/jmoletteire/Mapping-Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
+
+// Grabbing our GeoJSON data
+d3.json(airportData).then(function(data) {
+    // Creating a GeoJSON layer with the data
+    L.geoJSON(data, {
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(`<h3>Airport code: ${feature.properties.faa}</h3><hr><h3>Airport name: ${feature.properties.name}</h3>`)
+       }
+    }).addTo(map);
+})
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
